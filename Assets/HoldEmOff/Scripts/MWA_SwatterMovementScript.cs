7046211e08278ae3baec37e16f7f800fc088a0bf
MirzaWilayatAli <mirza.wilayat.ali148@gmail.com>
2024-10-27 03:17:39 +1100

    Swatter can now swat
    Swatter (block out) can now squash the bugs aka Delete them

--- Assets/HoldEmOff/Scripts/MWA_SwatterMovementScript.cs
+++ Assets/HoldEmOff/Scripts/MWA_SwatterMovementScript.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class MWA_SwatterMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // List to track insects in the swatter's collider area
+    private List<Collider2D> insectsInRange = new List<Collider2D>();
 
     // Update is called once per frame
     void Update()
@@ -24,5 +21,26 @@ public class MWA_SwatterMovementScript : MonoBehaviour
 
         // Update the position of the swatter to follow the mouse
         transform.position = worldPosition;
+
+        // Check if the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Check again to make sure the list is not empty before accessing
+            if (insectsInRange.Count > 0)
+            {
+                // Squash the first insect in the list
+                Destroy(insectsInRange[0].gameObject);
+                insectsInRange.RemoveAt(0); // Remove it from the list after squashing
+            }
+        }
+    }
+
+    // Called when an insect enters the swatter's trigger area
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Insect") && !insectsInRange.Contains(other))
+        {
+            insectsInRange.Add(other);
+        }
     }
 }
